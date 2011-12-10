57ef65cf25842d4bacae571e2d1eb725296003c4
Developer on node.newleafdigital.com <ben@newleafdigital.com>
2011-12-10 16:14:23 -0500

    not working

--- hq.js
+++ hq.js
@@ -6,6 +6,13 @@
 var http = require('http'),
     httpProxy = require('http-proxy');
 
+/
+// Create your proxy server
+//
+httpProxy.createServer(3000, 'localhost').listen(80);
+
+
+/*
 var proxyTable = {
   router: {
     '/sharelist' : '127.0.0.1:3000'
@@ -14,3 +21,4 @@ var proxyTable = {
 
 var proxyServer = httpProxy.createServer(proxyTable);
 proxyServer.listen(80);
+*/
