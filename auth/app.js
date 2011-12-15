8fc4513f94b11c01cf86b02fb8ac4e0cb8f2ade1
Developer on node.newleafdigital.com <ben@newleafdigital.com>
2011-12-14 20:16:49 -0500

    deleted .gitignores in submodules, want to track everything

--- auth/app.js
+++ auth/app.js
@@ -305,3 +305,6 @@ if (! module.parent) {
   app.listen(80);
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 }
+else {
+  console.log('auth module has parent, not listening.');
+}
